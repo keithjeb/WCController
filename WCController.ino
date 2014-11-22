d90d295cf6becaaed236ddc1f660a99fe6f3630a
keithjeb <keith.j.bailey@gmail.com>
2014-11-22 21:48:51 +0000

    test fandiag

--- WCController.ino
+++ WCController.ino
@@ -42,8 +42,8 @@ LiquidCrystal_I2C	lcd(I2C_ADDR, En_pin, Rw_pin, Rs_pin, D4_pin, D5_pin, D6_pin, 
 //=================================================
 // Forward declaration of the menu elements
 extern M2tk m2;
-M2_EXTERN_ALIGN(el_fan_diag);
-M2_EXTERN_ALIGN(el_cont_diag);
+//M2_EXTERN_ALIGN(el_fan_diag);
+//M2_EXTERN_ALIGN(el_cont_diag);
 
 //Some waiting variables for the various loops
 uint8_t temp_delay;
@@ -88,10 +88,50 @@ uint8_t fan_diag_abs_temp;
 uint8_t fan_diag_linked_sensor;
 
 
+//setup dialog
+M2_LABEL(el_fan_diag_l1, NULL, "Temp Control");
+M2_TOGGLE(el_fan_diag_t1, "", &fan_diag_temp_ctrl);
+M2_LABEL(el_fan_diag_l2, NULL, "Start Delta");
+M2_U8NUM(el_fan_diag_u1, "c2", 0, 99, &fan_diag_start_temp);
+M2_LABEL(el_fan_diag_l3, NULL, "Max Temp");
+M2_U8NUM(el_fan_diag_u2, "c2", 0, 99, &fan_diag_full_temp);
+M2_LABEL(el_fan_diag_l4, NULL, "Min Duty");
+M2_U8NUM(el_fan_diag_u3, "c3", 0, 100, &fan_diag_min_duty);
+M2_LABEL(el_fan_diag_l5, NULL, "Abs Max Temp");
+M2_U8NUM(el_fan_diag_u4, "c3", 0, 100, &fan_diag_abs_temp);
+M2_LABEL(el_fan_diag_l6, NULL, "Linked Sensor");
+M2_COMBO(el_fan_diag_c1, NULL, &fan_diag_linked_sensor, 4, fn_idx_to_sensor);
+M2_BUTTON(el_fan_diag_btn, "", "Back", back_to_menu);
+M2_LIST(fandiaglist1) = {
+	&el_fan_diag_l1, &el_fan_diag_t1, &el_fan_diag_l2, &el_fan_diag_u1, &el_fan_diag_l3, &el_fan_diag_u2, &el_fan_diag_l4, &el_fan_diag_u3, &el_fan_diag_l5, &el_fan_diag_u4, &el_fan_diag_l6,&el_fan_diag_c1, &el_fan_diag_btn);
+
+M2_GRIDLIST(el_fan_diag_grid, "c2", fandiaglist);
+M2_VSB(el_fan_diag_vsb, "w16h4l4", 0, 7);
+M2_LIST(fandiaglist2) = { el_fan_diag_grid, el_fan_diag_vsb);
+M2_HLIST(el_fan_diag, "", &elfandiaglist2);
 
 
+void back_to_menu(m2_el_fnarg_p fnarg) {
+}
 
+const char *fn_idx_to_sensor(uint8_t idx) {
+}
 
+void setup() {
+	m2_SetNewLiquidCrystal(&lcd, 20, 4);
+	// define button for the select message
+	m2.setPin(M2_KEY_SELECT, BTNPIN);
 
-
+	// The incremental rotary encoder is conected to these two pins
+	m2.setPin(M2_KEY_ROT_ENC_A, DTPIN); //DT
+	m2.setPin(M2_KEY_ROT_ENC_B, CLKPIN); //CLK
+	//set the backlight pin, but let the backlight worker update it.
+	lcd.setBacklightPin(BACKLIGHT_PIN, POSITIVE);
+}
+void loop() {
+	m2.checkKey();
+	if (m2.handleKey()) {
+		m2.draw();
+	}
+	}
 
